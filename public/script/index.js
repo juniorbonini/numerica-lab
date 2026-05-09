d1b8c3ed0ccb314f50287efd4880928c1a5c4edd
juniorbonini <juniorboninia7x@gmail.com>
2026-05-09 20:21:45 -0300

    (js): declare game state variables

--- public/script/index.js
+++ public/script/index.js
@@ -2,3 +2,8 @@ const maxAttempts = 10;
 const minNumber = 1;
 const maxNumber = 100;
 
+const randomNumber = (min, max) => Math.floor(Math.random() * max) + min;
+const secretNumber = randomNumber(1, 100);
+
+let secret = /* Function em andamento */;
+let attemptsLeft = maxAttempts;

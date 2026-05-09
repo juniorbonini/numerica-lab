7947773c6816518b960388f48282ebef2de89ad8
juniorbonini <juniorboninia7x@gmail.com>
2026-05-09 20:34:48 -0300

    (js): add showMessage function to update feedback text

--- public/script/index.js
+++ public/script/index.js
@@ -5,15 +5,24 @@ const maxNumber = 100;
 const randomNumber = (min, max) => Math.floor(Math.random() * max) + min;
 const secretNumber = randomNumber(1, 100);
 
-let secret = /* Function em andamento */;
+let secret = generateSecret();
 let attemptsLeft = maxAttempts;
 
 const getElement = (id) => document.getElementById(id);
 const getValue = (id) => getElement(id).value;
-const clearField = () => getElement("guessInput").value = "";
-const numbetInt = (value) => parseInt(value, 10)
-const isValidNumber = (number) => isNaN(number) || number < minNumber || number > maxNumber;
+const clearField = () => (getElement("guessInput").value = "");
+const numbetInt = (value) => parseInt(value, 10);
+const isValidNumber = (number) =>
+  isNaN(number) || number < minNumber || number > maxNumber;
 const isRightSecret = (number) => number === secret;
 const numberIsHigher = (number) => number > secret;
 const noAttempts = () => attemptsLeft === 0;
 
+function generateSecret() {
+  Math.floor(Math.random() * maxNumber) + minNumber;
+}
+
+function showMessage(message) {
+  const msg = (getElement("feedbackText").textContent = message);
+  return msg;
+}
